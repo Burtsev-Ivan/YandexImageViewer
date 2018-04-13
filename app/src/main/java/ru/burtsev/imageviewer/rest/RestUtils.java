f2f3e38fea6932ecb1aa2ce9c6120f59705a0345
Бурцев Иван Владимирович <Buran496@mail.ru>
2018-04-13 23:35:49 +0300

    add support TLS v1.2

--- app/src/main/java/ru/burtsev/imageviewer/rest/RestUtils.java
+++ app/src/main/java/ru/burtsev/imageviewer/rest/RestUtils.java
@@ -1,8 +1,17 @@
 package ru.burtsev.imageviewer.rest;
 
+import android.os.Build;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLContext;
+
+import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -35,11 +44,40 @@ public class RestUtils {
     }
 
     private static void initOkHttpClient() {
-        okHttpClient = new OkHttpClient.Builder()
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
                 .readTimeout(TIMEOUT, TimeUnit.SECONDS)
                 .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
-                .addInterceptor(new AccessTokenInterceptor())
+                .addInterceptor(new AccessTokenInterceptor());
+        okHttpClient = enableTls12OnPreLollipop(builder)
                 .build();
+
+    }
+
+    // Метод позволяющий использовать TLSv1.2 в андроиде младше api 22
+    // Без этого вылетает  SSLProtocolException
+    private static OkHttpClient.Builder enableTls12OnPreLollipop(OkHttpClient.Builder client) {
+        if (Build.VERSION.SDK_INT >= 16 && Build.VERSION.SDK_INT < 22) {
+            try {
+                SSLContext sc = SSLContext.getInstance("TLSv1.2");
+                sc.init(null, null, null);
+                client.sslSocketFactory(new Tls12SocketFactory(sc.getSocketFactory()));
+
+                ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                        .tlsVersions(TlsVersion.TLS_1_2)
+                        .build();
+
+                List<ConnectionSpec> specs = new ArrayList<>();
+                specs.add(cs);
+                specs.add(ConnectionSpec.COMPATIBLE_TLS);
+                specs.add(ConnectionSpec.CLEARTEXT);
+
+                client.connectionSpecs(specs);
+            } catch (Exception exc) {
+                Log.e("OkHttpTLSCompat", "Error while setting TLS 1.2", exc);
+            }
+        }
+
+        return client;
     }
 
     public static ApiService getApiService() {
